54f21a501d4275eb4870347063ce85c09bb02721
ppakdev <ppak73@gmail.com>
2014-07-15 08:40:23 -0400

    heroku

--- app/models/FFN.rb
+++ app/models/FFN.rb
@@ -1,6 +1,6 @@
 class FFN
 
-  def self.
+  def self.seed
     api_key = ENV['FFN_API_KEY']
     api_call = HTTParty.get("http://www.fantasyfootballnerd.com/service/players/json/")["Players"]
   end
